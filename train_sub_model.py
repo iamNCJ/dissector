3a6f35de65fc541b11f168492257a58490f14755
NCJ <me@ncj.wiki>
2022-08-14 16:39:47 +0800

    Update train_sub_model.py

--- train_sub_model.py
+++ train_sub_model.py
@@ -5,6 +5,7 @@ import pytorch_lightning as pl
 
 
 if __name__ == '__main__':
+    # init
     dm = CIFAR10DataModule('./data/cifar10')
     orig_model = ResNet20CIFAR10()
     orig_model.load_weights('./resnet20-12fca82f.th')
@@ -15,10 +16,14 @@ if __name__ == '__main__':
         'layer3.2.relu': 'feat_3',
     }
     sub_model = SubModel(orig_model, feat_dict, n_classes=10)
+
     # dry run to initialize LazyModules
+    dm.setup()
     dl = dm.train_dataloader()
     x, _ = next(iter(dl))
     sub_model(x)
+
+    # training
     trainer = pl.Trainer(accelerator='auto')
     trainer.fit(sub_model, dm)
     trainer.test(sub_model, dm)
